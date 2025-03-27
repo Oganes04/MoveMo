43bb2be4a659e37c176846bbcf1540653e566d66
Kavkazec <oganesmkrtchyan2004@gmail.com>
2025-03-27 20:23:22 +0300

    edit pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//====================== Обработка нажатия на кастомные чекбоксы ===================
+
 $(".check-label").on("click", function () {
     let isChecked = $(this).children("input").prop("checked");
     if (isChecked) {
@@ -6,3 +8,41 @@ $(".check-label").on("click", function () {
         $(this).find(".fakecheck").removeClass("checked");
     }
 });
+
+
+//====================== Обработка нажатия на бургер меню ===================
+
+$(".burger-menu-btn").click(function() {
+    $(this).find('.burger_menu').toggleClass('open');
+});
+
+
+
+//====================== Реализация функционала popup окон ===================
+
+function openPopup(popup) {
+    $('.popup').hide();
+    $('.overlay').show();
+    $('html').css('overflow', 'hidden');
+    popup.show();
+}
+  
+function closePopup(closeBtn) {
+    $('.overlay').hide();
+    closeBtn.parent().hide();
+    $('html').css('overflow-y', 'auto');
+}
+
+$(document).on('click', '.popup_close', function(e) {
+    closePopup($(this));
+});
+  
+
+$('.popup-account-change-btn').click(function() {
+    openPopup($('.popup-account-change'));
+});
+
+$('.popup-dept-btn').click(function() {
+    openPopup($('.popup-dept'));
+});
+
